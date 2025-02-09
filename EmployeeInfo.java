8c2f5684337d8bd486064381d593f024645b3930
KomalThakur20 <tkomal715@gmail.com>
2025-02-09 22:51:45 +0530

    Add files via upload

---
+++ EmployeeInfo.java
@@ -0,0 +1,11 @@
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+        System.out.println("NAME\tMAIL-ID\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Parul\tparul@cse\tp098988\t\t9849339389");
+        System.out.println("Sarita\tsarita@cse\tp098987\t\t9849339444");
+        System.out.println("Sarika\tsarika@cse\tp098986\t\t9849339222");
+        System.out.println("sara\tsara@cse\tp098982\t\t9849345689");
+        System.out.println("komal\tkomal@cse\tp098588\t\t9849331229");
+    }
+}
