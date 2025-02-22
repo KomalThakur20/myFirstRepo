beeffe86c2278c13a4cdd46f1daf899f1cbc6d10
KomalThakur20 <tkomal715@gmail.com>
2025-02-22 20:13:24 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,17 @@
+
+
+public class Average {
+
+   public static void main(String[] args) {
+    int april=12;
+    int may=14;
+    int june=8;
+    double Average;
+    double total =april+may+june;
+Average=total/3;
+    System.out.println("Synsets Entered april : " + april);
+    System.out.println("Synsets Entered may : " + may);
+    System.out.println("Synsets Entered june  :" + june);
+   }
+}
+
