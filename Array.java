56b3d2db3e043b24524b87945e0e6289ae8507b2
KomalThakur20 <tkomal715@gmail.com>
2025-02-22 20:13:01 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    
+    public static void main(String[] args) {
+        int A[]={1,2,3,4};
+        int n=A.length;
+        for(int i=0;i<n;i++)
+        {
+        System.out.println("Array is: "+A[i]);
+        }
+    }
+}
