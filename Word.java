920d39741e047bbb762ee32e0a599a82541b521f
KomalThakur20 <tkomal715@gmail.com>
2025-02-22 20:13:40 +0530

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,26 @@
+public class Word {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number==1)
+        {
+            System.out.println("ONE");
+        }
+        if (number==2)
+        {
+          System.out.println("TWO");  
+        }
+        if (number==3)
+        {
+            System.out.println("THREE");
+        }
+        if (number==4)
+        {
+            System.out.println("FOUR");
+        }
+        if (number==5)
+        {
+            System.out.println("FIVE");
+        }
+        
+    }
+}
